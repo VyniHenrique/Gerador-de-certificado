ba7b7359460027ba7d63577b0e86d0a5da694856
Vynicius <vynicius.henrique15@gmail.com>
2025-08-03 23:10:34 -0300

    criando componentes principais

---
+++ src/app/_components/remove-button/remove-button.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-remove-button',
+  imports: [],
+  templateUrl: './remove-button.html',
+  styleUrl: './remove-button.css'
+})
+export class RemoveButton {
+
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './_components/navbar/navbar';
+import { Title } from "./_components/title/title";
+import { PrimaryButton } from "./_components/primary-button/primary-button";
+import { SecondaryButton } from "./_components/secondary-button/secondary-button";
+import { RemoveButton } from "./_components/remove-button/remove-button";
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar],
+  imports: [Navbar, Title, PrimaryButton, RouterOutlet, SecondaryButton, RemoveButton],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
