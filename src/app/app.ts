a687331417b39d40bfb298caf1897851afae41f3
Vynicius <vynicius.henrique15@gmail.com>
2025-08-05 22:33:06 -0300

    criando a pagina principal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './_components/navbar/navbar';
-import { Title } from "./_components/title/title";
 import { PrimaryButton } from "./_components/primary-button/primary-button";
 import { SecondaryButton } from "./_components/secondary-button/secondary-button";
 import { RemoveButton } from "./_components/remove-button/remove-button";
 import { ItemCertificado } from "./_components/item-certificado/item-certificado";
+import { WhithoutCertificate } from "./pages/whithout-certificate/whithout-certificate";
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Title, PrimaryButton, RouterOutlet, SecondaryButton, RemoveButton, ItemCertificado],
+  imports: [Navbar, PrimaryButton, RouterOutlet, SecondaryButton, RemoveButton, ItemCertificado, WhithoutCertificate],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/pages/whithout-certificate/whithout-certificate.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SecondaryButton } from "../../_components/secondary-button/secondary-button";
+
+@Component({
+  selector: 'app-whithout-certificate',
+  imports: [SecondaryButton],
+  templateUrl: './whithout-certificate.html',
+  styleUrl: './whithout-certificate.css'
+})
+export class WhithoutCertificate {
+
+}
