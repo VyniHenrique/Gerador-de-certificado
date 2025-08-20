27add8bf692bfe0804a7cce863ac00789ffa80c8
Vynicius <vynicius.henrique15@gmail.com>
2025-08-19 21:27:10 -0300

    adicionando as rotas da aplicacao

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { WhithoutCertificate } from './pages/whithout-certificate/whithout-certificate';
+import { FormCertificate } from './pages/form-certificate/form-certificate';
+import { Certificate } from './pages/certificate/certificate';
+import { WithCertificate } from './pages/with-certificate/with-certificate';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: WithCertificate
+  },
+  {
+    path: "certificate/new",
+    component: FormCertificate
+  },
+  {
+    path: "certificate/:id",
+    component: Certificate
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './_components/navbar/navbar';
-import { PrimaryButton } from "./_components/primary-button/primary-button";
-import { SecondaryButton } from "./_components/secondary-button/secondary-button";
-import { RemoveButton } from "./_components/remove-button/remove-button";
-import { ItemCertificado } from "./_components/item-certificado/item-certificado";
-import { WhithoutCertificate } from "./pages/whithout-certificate/whithout-certificate";
-import { WithCertificate } from "./pages/with-certificate/with-certificate";
-import { FormCertificate } from "./pages/form-certificate/form-certificate";
-import { Certificate } from "./pages/certificate/certificate";
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, PrimaryButton, RouterOutlet, SecondaryButton, RemoveButton, ItemCertificado, WhithoutCertificate, WithCertificate, FormCertificate, Certificate],
+  imports: [Navbar, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
